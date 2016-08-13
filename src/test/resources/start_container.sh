68f73914db09d7c1a92d33b4e8beba6f0cb70eae
Robert Brem <brem_robert@hotmail.com>
2016-08-13 13:12:57 +0200

    start zookeeper und kafka

--- src/test/resources/start_container.sh
+++ src/test/resources/start_container.sh
@@ -5,8 +5,8 @@ VERSION=0.0.1
 echo "stop and rm keycloak"
 docker stop keycloak && docker rm keycloak
 
-echo "start keycloak"
 # -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin \
+echo "start keycloak"
 docker run --name keycloak -d \
 -v /root/volumes/keycloakdata:/opt/jboss/keycloak/standalone/data \
 -p 8088:8080 -p 8443:8443 robertbrem/keycloak:1.9.5
@@ -33,6 +33,31 @@ docker cp initial_db.sql cassandra:/
 echo "create database"
 docker exec -d cassandra cqlsh localhost -f /initial_db.sql
 
+echo "stop and rm kafka"
+docker stop kafka && docker rm kafka
+
+echo "stop and rm zookeeper"
+docker stop zookeeper && docker rm zookeeper
+
+echo "start zookeeper"
+docker run --name zookeeper -d \
+-p 2181:2181 \
+-e ZOOKEEPER_ID="1" \
+-e ZOOKEEPER_SERVER_1=kafka-zoo-svc \
+digitalwonderland/zookeeper
+
+echo "start kafka"
+docker run --name kafka -d -p 9092:9092 \
+-e ENABLE_AUTO_EXTEND="true" \
+-e KAFKA_RESERVED_BROKER_MAX_ID="999999999" \
+-e KAFKA_AUTO_CREATE_TOPICS_ENABLE="false" \
+-e KAFKA_PORT="9092" \
+-e KAFKA_ADVERTISED_PORT="9092" \
+-e KAFKA_CREATE_TOPICS="pokertracker:16:1" \
+-e KAFKA_ADVERTISED_HOST_NAME=`ip addr show docker0 | grep "scope global" | awk '{print $2}' | sed 's/\/.*//'` \
+-e KAFKA_ZOOKEEPER_CONNECT=`ip addr show docker0 | grep "scope global" | awk '{print $2}' | sed 's/\/.*//'`:2181 \
+cloudtrackinc/kubernetes-kafka
+
 echo "copy files for pokertracker"
 rm -r pokertracker-docker
 mkdir pokertracker-docker
@@ -43,7 +68,10 @@ cp standalone.xml pokertracker-docker
 echo "build image"
 docker build -t registry:5000/robertbrem/pokertracker:$VERSION pokertracker-docker
 docker stop pokertracker && docker rm pokertracker
-docker run -d -p 8282:8080 --name pokertracker -e CASSANDRA_IP=`ip addr show docker0 | grep "scope global" | awk '{print $2}' | sed 's/\/.*//'` registry:5000/robertbrem/pokertracker:$VERSION
+docker run -d -p 8282:8080 --name pokertracker \
+-e CASSANDRA_ADDRESS=`ip addr show docker0 | grep "scope global" | awk '{print $2}' | sed 's/\/.*//'` \
+-e KAFKA_ADDRESS=`ip addr show docker0 | grep "scope global" | awk '{print $2}' | sed 's/\/.*//'`:9092 \
+registry:5000/robertbrem/pokertracker:$VERSION
 echo "Docker container running with name pokertracker"
 
 echo " wait for wildfly to start"
